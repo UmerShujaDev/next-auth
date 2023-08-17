12c29705a5cb40e5aae35014412b887ca734f8ed
umerShujaDev <umershuja12@gmail.com>
2023-08-17 12:31:31 +0500

    fixed

--- src/helpers/getDataFromtoken.ts
+++ src/helpers/getDataFromtoken.ts
@@ -1,16 +1,13 @@
-import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
 
-import jwt from "jsonwebtoken"
-import { toast } from "react-hot-toast";
-
-export const getDataFromToken = (request: NextResponse) => {
+export const getDataFromToken = (request: NextRequest) => {
     try {
-        const token = request.cookies.get("token")?.value || ""
-        const decodeToken: any = jwt.verify(token, process.env.TOKEN_SECRET!)
-        return decodeToken.id
-    } catch (error:any) {
-        throw new Error(error.message)
-        
+        const token = request.cookies.get("token")?.value || '';
+        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+        return decodedToken.id;
+    } catch (error: any) {
+        throw new Error(error.message);
     }
 
 }
